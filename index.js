a4d8ffdd3d231ca7d9e5452f837718c75a9f8399
arshalD <60282895+arshalD@users.noreply.github.com>
2022-11-15 15:57:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
+    const fruitsList = document.querySelector("#fruits");
+    const grapesListItem = document.createElement("li");
+
+    // add classes
+    grapesListItem.classList.add("list-item");
+
+    // add attributes
+    grapesListItem.setAttribute("id", "grapes");
+
+    // add events
+    grapesListItem.addEventListener("click", () => {
+        alert("You clicked on Grapes!");
+    });
+
+    fruitsList.appendChild(grapesListItem);
     console.log("Just got a request!")
     res.send('Yo!')
 })
